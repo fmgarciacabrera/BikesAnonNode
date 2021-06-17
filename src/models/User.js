9fa0b2a789bc25f982026f4ad9a948a5b95415ef
Manuel Hernández <manu.hdez.dev@gmail.com>
2021-06-17 12:33:53 +0100

    feat: add mongoose user model with basic data

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+import * as mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  }
+}, {
+  toJSON: {
+    transform: (doc, ret) => {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
+      delete ret.password;
+    }
+  }
+})
+
+export default mongoose.model('User', UserSchema);
